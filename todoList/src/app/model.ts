6832da251919925c98c57e68c89cd8214cd50dbc
KamilMarkuszewski <kajt_sweb@tlen.pl>
2019-01-06 14:58:51 +0100

    create model class in vanilla js

---
+++ todoList/src/app/model.ts
@@ -0,0 +1,12 @@
+var model = 
+{
+    user: "UserName",
+    items: 
+    [
+        {action: "Task 1", done: false},
+        {action: "Task 2", done: false},
+        {action: "Task 3", done: true},
+        {action: "Task 4", done: true},
+    ]
+
+}
