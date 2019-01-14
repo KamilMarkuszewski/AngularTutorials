f4651ae8324cc5718f2acf0b7682e41b6f0fe42f
KamilMarkuszewski <kajt_sweb@tlen.pl>
2019-01-14 14:03:04 +0100

    zero filozofii w tym EF

--- entityFramework/EF_Tutorial/EF_Tutorial/Program.cs
+++ entityFramework/EF_Tutorial/EF_Tutorial/Program.cs
@@ -10,8 +10,64 @@ namespace EF_Tutorial
     {
         static void Main(string[] args)
         {
+            AddStudentsSeedIfNeeded();
+
+            ShowAllStudents();
+
+            ClearStudentsTable();
+
+            ShowAllStudents();
+
+
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
+
+        private static void ClearStudentsTable()
+        {
+            using (CoreDbContext context = new CoreDbContext())
+            {
+                var students = context.Students.ToArray();
+
+                foreach (var objectToDelete in students)
+                {
+                    context.Students.Remove(objectToDelete);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        private static void ShowAllStudents()
+        {
+            Console.WriteLine("------------");
+            Console.WriteLine("Students: ");
+            using (CoreDbContext context = new CoreDbContext())
+            {
+                foreach (var student in context.Students)
+                {
+                    Console.WriteLine($"Student: {student.FirstName} {student.LastName}");
+                }
+            }
+            Console.WriteLine("------------");
+        }
+
+        private static void AddStudentsSeedIfNeeded()
+        {
+            using (CoreDbContext context = new CoreDbContext())
+            {
+                if (!context.Students.Any())
+                {
+                    var student = new Student()
+                    {
+                        FirstName = "Kamil",
+                        LastName = "Markuszewski",
+                        EnrollmentDate = new DateTime(2019, 1, 14)
+                    };
+                    context.Students.Add(student);
+                    context.SaveChanges();
+                }
+            }
+        }
+
     }
 }
